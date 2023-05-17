059d94a61178f8b25742bee74b36c3fa7dadca95
Jacob Beard <Jacob.Beard@nttdata.com>
2023-05-17 10:50:25 -0500

    Added comments for Group.js

--- 2/js/Group.js
+++ 2/js/Group.js
@@ -3,6 +3,7 @@
 GROUP!
 
 **********************************/
+//Makes 7 different group color options
 Group.COLORS = {
 	"-1":"#000000", // black
 	0: "#880000", // red
@@ -15,6 +16,7 @@ Group.COLORS = {
 Group.FONTSIZE = 40;
 Group._CLASS_ = "Group";
 
+//Appears to make "groups" of objects to classify/seperate them, but doesn't appear to be in use anywhere?
 function Group(model, config){
 
 	const self = this;
@@ -30,18 +32,21 @@ function Group(model, config){
 		x: 0,
 			y: 0,
 	};
+	//Creates type index for a group
 	injectedDefaultProps(defaultProperties,objTypeToTypeIndex("Group"));
 	_configureProperties(self, config, defaultProperties);
 
-	// Draw
+	// Draws a group
 	self.draw = function(ctx){
-
+		//*Can we console.log self.visibility to see if a group is ever visible??
+		//Checks if a group is meant to be visibile and whether its in play or edit mode
 		if(self.visibility===1 && self.loopy.mode===Loopy.MODE_PLAY) return;
-		// Retina
+		// Retina; if group is drawn, make sure it renders correctly
 		const x = self.x*2;
 		const y = self.y*2;
 
 		// DRAW HIGHLIGHT???
+		//Specifically, draws a highlight around the group if its selected in the sidebar
 		if(self.loopy.sidebar.currentPage.target === self){
 			const bounds = self.getBounds();
 			ctx.save();
@@ -58,12 +63,14 @@ function Group(model, config){
 		ctx.translate(x,y);
 
 		// Text!
+		//Sets text [label?] for a group
 		ctx.font = "100 "+Group.FONTSIZE+"px sans-serif";
 		ctx.textAlign = "center";
 		ctx.textBaseline = "middle";
 		ctx.fillStyle = Group.COLORS[self.textColor];
 
 		// ugh new lines are a PAIN.
+		//Renders and formats lines in a group as intended (using the "lines" array)
 		const lines = self.breakText();
 		ctx.translate(0, -(Group.FONTSIZE*lines.length)/2);
 		for(let i=0; i<lines.length; i++){
@@ -81,6 +88,9 @@ function Group(model, config){
 	// KILL Group /////////////////////////
 	//////////////////////////////////////
 
+
+	//Removes a group from its parent and deletes the group
+	//*Should this be "parents"? Is a group unique to one object, or should it be more than 1?
 	self.kill = function(){
 
 		// Remove from parent!
@@ -95,10 +105,12 @@ function Group(model, config){
 	// HELPER METHODS ////////////////////
 	//////////////////////////////////////
 
+	//Splits text into new lines based on newline character
 	self.breakText = function(){
 		return self.text.split(/\n/);
 	};
 
+	//Calculates the bounds of the box that would encompass a group
 	self.getBounds = function(){
 
 		const ctx = self.model.context;
@@ -126,10 +138,13 @@ function Group(model, config){
 
 	};
 
+	//Checks whether a given coordinate is within a group box
 	self.isPointInGroup = function(x, y){
 		return _isPointInBox(x,y, self.getBounds());
 	};
 
+	//Gives coordinates of the bounding box used to denote a group
+	//*Difference between getBounds and getBoundingBox functions??
 	self.getBoundingBox = function(){
 		const bounds = self.getBounds();
 		return {
